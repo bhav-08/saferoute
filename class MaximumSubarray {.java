8efa7eb7284cae5d025881277abe4ba805744a2e
bhav-08 <bhavanikatam2008@gmail.com>
2026-09-11 11:04:27 +0530

    Add Assignment 4  STEP-5 java programs

---
+++ class MaximumSubarray {.java
@@ -0,0 +1,28 @@
+class MaximumSubarray {
+
+    static int maxSubArray(int[] nums) {
+
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            // Decide: extend or start new subarray
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+
+            // Update maximum sum
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        int result = maxSubArray(nums);
+
+        System.out.println("Maximum Subarray Sum: " + result);
+    }
+}
---
+++ class SubarraySumK {.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+class SubarraySumK {
+
+    static int subarraySum(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int currentSum = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            currentSum = currentSum + nums[i];
+
+            if (map.containsKey(currentSum - k)) {
+                count = count + map.get(currentSum - k);
+            }
+
+            if (map.containsKey(currentSum)) {
+                map.put(currentSum, map.get(currentSum) + 1);
+            } else {
+                map.put(currentSum, 1);
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 1};
+        int k = 2;
+
+        int result = subarraySum(nums, k);
+
+        System.out.println("Number of subarrays: " + result);
+    }
+}
